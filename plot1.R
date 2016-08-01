b60530028d92083531f715a791d6f9f30c8bb8b4
multicad <kim.hakju@gmail.com>
2016-08-01 14:39:52 +0900

    project
    assessment

---
+++ plot1.R
@@ -0,0 +1,15 @@
+## Getting full dataset
+dataFile <- "./household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+## Subsetting the data
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+
+## Saving to file
+png("plot1.png", width=480, height=480)
+
+## Plot
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,16 @@
+## Getting full dataset
+dataFile <- "./household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+## Subsetting the data
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+
+## Saving to file
+png("plot2.png", width=480, height=480)
+
+## Plot
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+dev.off()
